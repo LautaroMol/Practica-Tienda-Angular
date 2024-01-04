42d620e1581c7ce26493a29c3fc39765888c6de1
Lautaro <laumol159@gmail.com>
2024-01-04 15:36:11 -0300

    terminado

--- FrontEndTienda/src/app/venta/venta.component.ts
+++ FrontEndTienda/src/app/venta/venta.component.ts
@@ -14,6 +14,9 @@ import {MatListModule} from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import {MatExpansionModule} from '@angular/material/expansion'; 
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AppComponent } from '../app.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-venta',
@@ -27,17 +30,21 @@ export class VentaComponent implements OnInit {
 
   constructor(
     private _tipoServicio: TipoService,
-    private _productoServicio: ProductoService,){}
+    private _productoServicio: ProductoService,
+    private _snackBar: MatSnackBar,
+    private appComponent: AppComponent,
+    private router: Router,
+    ){}
     listaProductos: Producto[] = [];
     listaCategorias: Tipo[] = [];
     carrito: Producto[] = [];
     selectedCategory: number | null = null;
     filteredProducts: Producto[] = [];
-
-  
+    
   ngOnInit() {
     this.mostrarProductos();
     this.mostrarCategorias();
+    this.filtrarProductosPorCategoria();
   }
 
   agregarAlCarrito(producto: Producto) {
@@ -54,6 +61,49 @@ export class VentaComponent implements OnInit {
   carritoVisible: boolean = false;
   toggleCarrito() {
     this.carritoVisible = !this.carritoVisible;
+  }
+
+  registrarVenta(carrito: Producto[]){
+    if (carrito.length === 0) {
+      this.mostrarSnackBar('El carrito está vacío. Agrega productos antes de finalizar la compra.');
+      return;
+    }
+    this.mostrarProductos();
+    carrito.forEach(product => {
+      const productoBD = this.listaProductos.find((productoLista ) => productoLista .idProducto === product.idProducto);
+      if (productoBD){
+        productoBD.ventas += product.ventas;
+        productoBD.stock -= product.ventas;
+        this._productoServicio.update(productoBD.idProducto,productoBD).subscribe({
+          next:(data) =>{
+            this.mostrarSnackBar('Venta finalizada con éxito');
+          },error:(e)=>{
+          }
+        })
+      }
+    })
+  }
+
+  showCards = false;
+  volverMenu(){
+    this.appComponent.showCards = true;
+    this.router.navigate(['/']);
+  }
+
+  mostrarSnackBar(mensaje: string) {
+    this._snackBar.open(mensaje, 'Cerrar', {
+      duration: 2000,
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
+    this.mostrarProductos();
+  }
+
+  eliminarProductoDelCarrito(producto: Producto) {
+    const index = this.carrito.indexOf(producto);  
+    if (index !== -1) {
+      this.carrito.splice(index, 1);
+    }
   }
 
   mostrarProductos() {
@@ -83,6 +133,19 @@ export class VentaComponent implements OnInit {
     this.filteredProducts = this.listaProductos.filter(producto => {
       return this.selectedCategory ? producto.codTipo === this.selectedCategory : true;
     });
+
+    console.log(this.filteredProducts);
   }
+
+  getGridColumns(): number {
+    const isFilteredProductsOdd = this.filteredProducts.length % 2 !== 0;
+    const isListaProductosOdd = this.listaProductos.length % 2 !== 0;
+  
+    if ((this.selectedCategory !== null && isFilteredProductsOdd) || (this.selectedCategory === null && isListaProductosOdd)) {
+      return 1;
+    }
+    return 2;
+  }
+
 }
 
